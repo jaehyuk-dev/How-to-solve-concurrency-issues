a89124a20906de86f9eafa442e715f5becc6fa36
jaehyuk <ljh@nis.co.kr>
2025-06-10 14:46:25 +0900

    재고감소 로직 작성

---
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,36 @@
+package com.example.stock.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Stock {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+
+    private Long quantity;
+
+    public Stock() {}
+
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void decrease(Long quantity) {
+        if(this.quantity < quantity) {
+            throw new IllegalArgumentException("Quantity exceeds stock limit");
+        } else {
+            this.quantity -= quantity;
+        }
+    }
+}
