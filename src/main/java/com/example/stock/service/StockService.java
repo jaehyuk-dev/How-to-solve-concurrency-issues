f24c545b957f1b2f608e6459fdbb60b0e3353ca9
jaehyuk <ljh@nis.co.kr>
2025-06-10 15:46:11 +0900

    NamedLock을 이용한 해결

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -4,6 +4,7 @@ import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -15,8 +16,8 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-//    @Transactional
-    public synchronized void decrease(Long id, Long quantity) {
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void decrease(Long id, Long quantity) {
         // synchronized를 사용하면 해당 메소드는 하나의 스레드만 접근 가능
         Stock stock = stockRepository.findById(id).orElseThrow(EntityNotFoundException::new);
         stock.decrease(quantity);
